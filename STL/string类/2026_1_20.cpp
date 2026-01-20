f7bdb8aa42236a1b3d152498b1d343b461b8046a
Li2ladybro <3258347687@qq.com>
2026-01-20 21:22:34 +0800

    Add files via upload

---
+++ STL/string类/2026_1_20.cpp
@@ -0,0 +1,54 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	string s1 = "Hello Word";
+	cout << s1 << endl;
+		auto s2 = s1.c_str();
+		cout << s2 << endl;
+
+		//for (unsigned char i = 0; i < 255; i++)
+		//{
+		//	cout << i << ' ';
+		//}
+
+		char str2[] = "丽康华";
+		cout << str2 << endl;
+		str2[5] = -45;
+		cout << str2 << endl; str2[5] = -46;
+		cout << str2 << endl; str2[5] = -47;
+		cout << str2 << endl; str2[5] = -48;
+		cout << str2 << endl; str2[5] = -49;
+		cout << str2 << endl; str2[5] = -50;
+		cout << str2 << endl; str2[5] = -51;
+		cout << str2 << endl;
+
+
+
+		str2[5] = -52;
+		cout << str2 << endl;
+		str2[5] = -53;
+		cout << str2 << endl;
+		str2[5] = -54;
+		cout << str2 << endl;
+		str2[5] = -55;
+		cout << str2 << endl;
+		str2[5] = -56;
+		cout << str2 << endl;
+		str2[5] = -57;
+		cout << str2 << endl;
+		str2[5] = -58;
+		cout << str2 << endl;
+
+		str2[5] = -59;
+		cout << str2 << endl;
+
+		str2[5] = -60;
+		cout << str2 << endl;
+
+		
+
+	return 0;
+}
